9fb8c235d8c8727ffea5748d7701359af203d260
Katy Cassidy <ihatetoast@gmail.com>
2025-12-08 17:32:50 -0600

    player lose logic done. player win logic done. reset button resets.

--- src/App.jsx
+++ src/App.jsx
@@ -10,36 +10,61 @@ function App() {
      const randoIdx = Math.floor(Math.random() * breedData.length);
     return (breedData[randoIdx]);
   });  
-  const [missedLetters, setMissedLetters] = useState([]);
-  const gameIsOver = missedLetters.length >=6;
+
+  const [missedLetters, setMissedLetters] =useState([]);
+  const [wordGuessed, setWordGuessed] = useState(false)
+  const [gameIsOver, setGameIsOver] = useState(false); // needs to handle also game is one
 
 const getBreed = () => {
   const randoIdx = Math.floor(Math.random() * breedData.length);
   return (breedData[randoIdx]);
 }
 
-const noNameYet =(missedLetter)=> {
-setMissedLetters((prevMissedLetters) => {
-        return [missedLetter, ...prevMissedLetters]})
+const handleMissedLetter =(missedLetter)=> {
+  setMissedLetters((prevMissedLetters) => {
+  return [missedLetter, ...prevMissedLetters]})
 }
 
-// reset to ... reset but also on init load. 
-const resetGame = () =>{
-setBreed(getBreed())
-setMissedLetters([]);
+const handleWordSolved = (wordSolved)=>{
+  setWordGuessed(wordSolved)
 }
-// when game is over, switch scoreboard with facts. last.
-console.log(missedLetters);
-console.log("game is over: ", gameIsOver);
+
+// todo: now fetch the facts. todo!
+const handleEndOfGame =()=>{
+  if(missedLetters.length === 6 || wordGuessed) {
+    setGameIsOver(true)
+  }
+}
+useEffect(() =>{
+  handleEndOfGame();
+},[missedLetters, wordGuessed])
+
+
+// reset after loss 
+const resetGame = () =>{
+  setBreed(getBreed())
+  setMissedLetters([]);
+  setWordGuessed(false);
+  setGameIsOver(false);
+}
+
+
+// remove even
+console.log("TESTING: WORD TO GUESS IS ", breed.name);
+const losingContent = <p>Oh no. You lost. The word was {breed.name}</p>;
+const winningContent = <p>Congrats! You guessed {breed.name} like a champ.</p>;
   return (
     <main>
       <section>
-        {!gameIsOver &&  <ScoreBoard mistakes={missedLetters}/>}
-        {gameIsOver && <p>some message dep on results. you won/lost. restart? button</p>}
-    
+        {!gameIsOver && <ScoreBoard mistakes={missedLetters}/>}
+        <div>
+          {gameIsOver && !wordGuessed && losingContent}
+          {gameIsOver && wordGuessed && winningContent}
+          {gameIsOver && <button onClick={resetGame}>Play Again</button>}
+        </div>
       </section>
       <section>
-        <HangdogGame breedName={breed.name} handleMissedLetters={noNameYet}/>
+        <HangdogGame key={breed.name} breedName={ breed.name.toUpperCase()} getMissedLetter={handleMissedLetter} getWordSolved={handleWordSolved}/>
       </section>
     </main>
   )
--- src/components/LettersComponents/HangdogGame.jsx
+++ src/components/LettersComponents/HangdogGame.jsx
@@ -1,63 +1,78 @@
-import {useState} from 'react'
+
+import {useState, useEffect} from 'react'
 
 import Key from './Key'
 
 import styles from './HangdogGame.module.css';
 
+// for keyboard, an array of arrays
+// may need to make one array and split at p, l, and m for validation. 
 const QUERTY_WITH_DIACRITICALS = [
   ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
   ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
   ['Z', 'X', 'C', 'V', 'B', 'N', 'M'],
   ['Ä','É', 'Ñ', 'Ö', 'Ü'] ];
 
-const HangdogGame = ({breedName, handleMissedLetters}) => {
-  const [playedLetters, setPlayedLetters] = useState([])
-
-  // arr of letters only (to compare clicks against)
-  const gameBreedLetters = breedName
-  .toUpperCase()
-  .replace(/[\s-]/g, "")
-  .split("");
-  console.log("gameBreedLetters: ", gameBreedLetters);
+const HangdogGame = ({breedName, getMissedLetter, getWordSolved}) => {
+  const [usedLetters, setUsedLetters] = useState([])
+  const [goodGuesses, setGoodGuesses] = useState([])
 
 
-  function gameSpaces(str) {
-    const spaces = str.toUpperCase().split("");
-    return spaces.map((space, idx) => {
-      if(space === " ") {
+  // create game spaces and include spaces and dashes of original breed name
+  const gameSpaces = () => {
+    return breedName.split('').map((letter, idx) => {
+      if(letter === " ") {
         return <span key={`blank-${idx}`} className={styles.blank}>{" "}</span>;
-      } else if (space === "-") {
+      } else if (letter === "-") {
         return <span key={`blank-${idx}`} className={styles.blank}>{"-"}</span>;
       } else {
-        return <span key={`letter-${idx}`} className={styles.letter}>{" - "}</span>;
+        return  <span key={`letter-${idx}`} className={styles.letter}>{goodGuesses.includes(letter) ? letter : " ? "}</span>;
       }
     })
   }
 
-
   function handleKeyClick(letter) {
-    setPlayedLetters((prevLetters)=>{
-      return [letter, ...prevLetters]
+    setUsedLetters((prevArr) => {
+      return [letter, ...prevArr]
     })
-    if(!gameBreedLetters.includes(letter)){
-    handleMissedLetters(letter)
+    // check if miss and return early
+    if(!breedName.includes(letter)){
+      getMissedLetter(letter);
+      return;
     }
+    // add to good guesses
+    setGoodGuesses((prevArr) => {
+      return [letter, ...prevArr]
+    })
+
   }
+ 
+
+
+useEffect(()=>{
+  const gameLettersOnly = breedName.replace(/[\s-]/g, "").split("");
+  const uniqueLetters = [...new Set(gameLettersOnly)]; // Use to remove duplicate elements from an array
+ // every letter in the unique letter arr is in the guessed letters arr
+  const allGuessed = uniqueLetters.every(letter => goodGuesses.includes(letter))
+  getWordSolved(allGuessed)
+}, [goodGuesses, breedName, getWordSolved])
+
+
+
+
 
   return (
     <div>
-      <p>for testing: in component: game word to guess is {breedName}</p>
       <div className={styles["game-tiles"]}>
-        {gameSpaces(breedName)}
-      </div>
-      
+        {gameSpaces()}
+      </div> 
       <div>
         {QUERTY_WITH_DIACRITICALS.map((row, rowIdx) => (
           <div className={styles.keyrow} key={rowIdx}>
-            {row.map(letter => <Key key={letter} letter={letter} onKeyClick={handleKeyClick} isDisabled={playedLetters.includes(letter)}>{letter}</Key>)}
+            {row.map(letter => <Key key={letter} letter={letter} onKeyClick={handleKeyClick} isDisabled={usedLetters.includes(letter)}>{letter}</Key>)}
           </div>
         ))}
-      </div>
+      </div> 
     </div>
   )
 }
